47af2d610dfcbe9c8f4ba6b41505226810797299
Al3xand3r11 <alexander.nance11@gmail.com>
2023-11-11 19:21:43 -0800

    First Commit

---
+++ src/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYy37M0tEXXeA0rsN-0SxlCOk1rjnSduA",
+  authDomain: "whatstheplay-app.firebaseapp.com",
+  projectId: "whatstheplay-app",
+  storageBucket: "whatstheplay-app.appspot.com",
+  messagingSenderId: "542259948242",
+  appId: "1:542259948242:web:50dd43cc2cc2b6d4c0db0b",
+  measurementId: "G-8CTF6GLNDT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
