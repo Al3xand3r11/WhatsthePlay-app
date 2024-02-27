9300602ed718e401e6a58b867b219c285d92167a
Al3xand3r11 <alexander.nance11@gmail.com>
2024-02-27 11:16:24 -0800

    members update

--- src/components/Members/index.jsx
+++ src/components/Members/index.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import icon1 from '../../images/profilepic.png';
+import Brian from '../../images/Brian.png';
+import Caleb from '../../images/Caleb.jpg';
+import CT from '../../images/CT.jpg';
+import Nate from '../../images/Nate.jpg'
+import JB from '../../images/JB.png';
+import JG from '../../images/JG.jpeg';
 import MemberBackground from '../../images/MembersBackground.png';
 import {
     Image,
@@ -21,42 +27,42 @@ const Members = () => {
         <MembersH1>Members</MembersH1>
         <MembersWrapper>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={JG} />
             <MembersH2>Jalen</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
             </MembersP>
           </MembersCard>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={JB} />
             <MembersH2>Demarco</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
             </MembersP>
           </MembersCard>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={CT} />
             <MembersH2>Christian</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
             </MembersP>
           </MembersCard>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={Nate} />
             <MembersH2>Nate</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
             </MembersP>
           </MembersCard>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={Caleb} />
             <MembersH2>Caleb</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
             </MembersP>
           </MembersCard>
           <MembersCard>
-            <MembersIcon src={icon1} />
+            <MembersIcon src={Brian} />
             <MembersH2>Brian</MembersH2>
             <MembersP>
             Lorem ipsum dolor sit amet
