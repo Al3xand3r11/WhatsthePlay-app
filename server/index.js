cf40742cf5219a81d7a73f99509e1db402f5afd9
Brandon Nance <113631937+Al3xand3r11@users.noreply.github.com>
2024-05-08 12:28:52 -0700

    Live stats (#4)
    * live stats working api
    
    * backend update
    
    * file deletion
    
    * live stats finished

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const PORT = 8000;
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv from 'dotenv';
+import date from 'date-and-time';
+
+const now = new Date();
+const fullDay = date.format(now,'YYYY-MM-DD')
+
+dotenv.config();
+
+
+const app = express()
+
+const corsOption = {
+    credentials: true,
+    origin: ['http://localhost:5173']
+}
+
+app.use(cors(corsOption));
+
+
+app.get('/', (req,res) => {
+    const config = {
+        method: 'get',
+        url: `https://v2.nba.api-sports.io/games?date=${fullDay}`,
+        headers: {
+          'x-rapidapi-key': process.env.REACT_API_KEY,
+          'x-rapidapi-host': 'v2.nba.api-sports.io'
+        }
+      };
+      
+      axios(config)
+      .then(function (response) {
+        res.json(response.data)
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+})
+
+app.listen(8000, () => console.log(`Server is running on port ${PORT}`))
---
+++ src/components/Stats/statsCall.jsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { useState, useEffect } from "react";
+import data from "./data";
+const StatsCall = ({}) => {
+  const [dailyStats, setDailyStats] = useState();
+
+  const pullStats = async() => {
+    const config = {
+      method: 'get',
+      url: 'http://localhost:8000/',
+    };
+     await axios(config)
+    .then(function (response) {
+      setDailyStats(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    }
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        pullStats();
+      }, 600000);
+      return () => clearInterval(interval);
+    },[])
+
+return(
+  
+  <div className="flex flex-col md:flex-row items-center justify-center  ">
+  <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
+  { dailyStats?.response.map((answer) => 
+  <div class="mx-auto my-2 max-w-md rounded overflow-hidden shadow-md text-xs pl-10">
+  <div class="flex bg-gray-200 px-2 py-2">
+    <div class="w-5/12 text-gray-700 text-left text-red-700">Quarter: {answer.periods.current}</div>
+    <div class="w-5/12 flex justify-end items-center">
+      <p class="w-8 px-2 text-center">1</p>
+      <p class="w-8 px-2 text-center">2</p>
+      <p class="w-8 px-2 text-center">3</p>
+      <p class="w-8 px-2 text-center">4</p>
+    </div>
+    <div class="w-1/6 text-gray-700 text-right">NBA</div>
+  </div>
+
+  <div class="flex px-2 py-2 items-center">
+    <div class="w-5/12 flex">
+        <div class="flex flex-col">
+        <p class="text-sm font-bold">{answer.teams.home.name}</p>
+      </div>
+    </div>
+    <div class="w-5/12 flex justify-end items-center">
+      <p class="w-8 px-1 text-center">{answer.scores.home.linescore[0]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.home.linescore[1]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.home.linescore[2]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.home.linescore[3]}</p>
+    </div>
+    <p class="w-1/6 text-lg sm:text-xl font-bold text-right">{answer.scores.home.points}</p>
+  </div>
+
+  <div class="flex px-2 py-2 items-center">
+    <div class="w-5/12 flex">
+      <div class="flex flex-col">
+        <p class="text-sm font-bold">{answer.teams.visitors.name}</p>
+      </div>
+    </div>
+    <div class="w-5/12 flex justify-end items-center">
+      <p class="w-8 px-1 text-center">{answer.scores.visitors.linescore[0]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.visitors.linescore[1]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.visitors.linescore[2]}</p>
+      <p class="w-8 px-1 text-center">{answer.scores.visitors.linescore[3]}</p>
+    </div>
+    <p class="w-1/6 text-lg sm:text-xl font-bold text-right">{answer.scores.visitors.points}</p>
+  </div>
+
+</div>
+)}
+</div>
+</div>
+
+
+)
+
+};
+
+export default StatsCall;
