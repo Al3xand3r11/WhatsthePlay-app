58759a63abfebe8ea9c9aad953b7ddcc5f671f30
Ray-823 <167665469+Ray-823@users.noreply.github.com>
2024-06-30 12:59:19 -0700

    mobile adaptation

--- src/components/Members/index.jsx
+++ src/components/Members/index.jsx
@@ -5,12 +5,12 @@ import Title from "../Title";
 
 const Members = () => {
     return (
-      <div id="Members" className="bg-member-image w-full h-screen bg-cover bg-center">
+      <div id="Members" className="bg-member-image w-full h-auto bg-cover bg-center">
       <div className="flex flex-col items-center">
       <Title>Members</Title>
       
       <div className="justify-between">
-            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pl-4 text-center md:text-start">
                 {members.map(project => (
                     <MemberItem
                         imgUrl={project.imgUrl}
